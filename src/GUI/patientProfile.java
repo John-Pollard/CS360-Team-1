e9d761e4af55f0f2f7bf0568a5b94ff38f51c09f
noelbarrera <70553615+noelbarrera@users.noreply.github.com>
2020-12-03 13:40:52 -0500

    init commit

---
+++ src/GUI/patientProfile.java
@@ -0,0 +1,107 @@
+package GUI;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class patientProfile implements Initializable {
+    @FXML
+    private TextField lastnamefield;
+
+    @FXML
+    private TextField middleField;
+
+    @FXML
+    private TextField firstnameField;
+
+    @FXML
+    private TextField ssnField;
+
+    @FXML
+    private TextField dateOfBirthField;
+
+    @FXML
+    private TextField address1Field;
+
+    @FXML
+    private TextField address2Field;
+
+    @FXML
+    private TextField cszField;
+
+    @FXML
+    private TextField doctorField;
+
+    @FXML
+    private TextField cellField;
+
+    @FXML
+    private TextArea notesFIeld;
+
+    @FXML
+    private ChoiceBox<?> sexBox;
+
+    @FXML
+    private TextField workField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField emnameField;
+
+    @FXML
+    private TextArea allergiesField;
+
+    @FXML
+    private TextField emrelationField;
+
+    @FXML
+    private TextField emcellField;
+
+    @FXML
+    private TextField emworkField;
+
+    @FXML
+    private Menu fileMenu;
+
+    @FXML
+    private Menu editMenu;
+
+    @FXML
+    private DatePicker firstvisitDate;
+
+    @FXML
+    private DatePicker lastvisitDate;
+
+    @FXML
+    private TextField roomField;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button prescriptionButton;
+
+    @Override
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+
+    }
+
+
+}

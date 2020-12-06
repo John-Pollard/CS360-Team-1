b3e32f7d479827b776ddb87eaeacb346ce78f38d
John Pollard <polljp01@pfw.edu>
2020-12-05 19:03:09 -0500

    Adding functionality to login page
    Started to add an action handler/Event handler to the button but for some reason can't get the button to be clickable?

--- src/GUI/login.java
+++ src/GUI/login.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -24,5 +26,23 @@ public class login implements Initializable {
         usernameField.setPromptText("username");
 
         passwordField.setPromptText("password");
+
+
+        signButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent a) {
+                //authenticate the username and password
+
+                try{
+                    System.out.println("Login Button Clicked"); // clicking the button isn't working
+                }catch(Exception e2){
+                    System.out.println("Invalid username or password");
+                }
+            }
+        });
+        System.out.println("Check 1"); // passed the check so it fully loads the page
     }
+
+
 }

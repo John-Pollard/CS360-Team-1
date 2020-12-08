c8cdc28d1f0d3ad6a304ce02504ba99587317716
noelbarrera <70553615+noelbarrera@users.noreply.github.com>
2020-12-08 16:12:37 -0500

    init commit

--- src/GUI/appointmentList.java
+++ src/GUI/appointmentList.java
@@ -10,8 +10,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -35,6 +37,12 @@ public class appointmentList implements Initializable {
 
     @FXML
     private TextField filterField;
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    private TableView<Employee> tableview;
 
     ObservableList<Employee> dataList = FXCollections.observableArrayList();
 

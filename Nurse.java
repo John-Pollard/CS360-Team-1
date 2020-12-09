dc3cd3447dd906ec84e60037e43941be93250246
audrab <73966815+audrab@users.noreply.github.com>
2020-12-08 19:50:28 -0500

    Add files via upload

---
+++ Nurse.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Nurse extends Person{
+    
+    ArrayList<String> patients = new ArrayList<String>();
+
+    public ArrayList<String> getPatients(){
+        return patients;
+    }
+    
+}
